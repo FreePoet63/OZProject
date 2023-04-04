650f84c481f067dfe49300ab618704b1d8012fda
Yury Razlivinsky <razlivinskiy1984@gmail.com>
2023-04-04 17:22:44 +0300

    init oz all methods

--- src/test/java/page/OzAllMethods.java
+++ src/test/java/page/OzAllMethods.java
@@ -2,6 +2,7 @@ package page;
 
 import com.codeborne.selenide.Condition;
 import io.qameta.allure.Step;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import selectors.AtlasWEbPage;
 import selectors.CollectionPage;
@@ -11,17 +12,22 @@ import java.time.Duration;
 import static com.codeborne.selenide.CollectionCondition.exactTexts;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static settings.StartSettings.driver;
 import static settings.StartSettings.onPage;
 
 public class OzAllMethods {
     @Step
     public static void searchElementPage(String strg) {
-        onPage(AtlasWEbPage.class).linkText(strg).click();
+        JavascriptExecutor ex = (JavascriptExecutor) driver;
+        WebElement element = onPage(AtlasWEbPage.class).linkText(strg);
+        ex.executeScript("arguments[0].click();", element);
     }
 
     @Step
     public static void clickButton(String text) {
-        onPage(AtlasWEbPage.class).button(text).click();
+        JavascriptExecutor ex = (JavascriptExecutor) driver;
+        WebElement element = onPage(AtlasWEbPage.class).button(text);
+        ex.executeScript("arguments[0].click();", element);
     }
 
     @Step
